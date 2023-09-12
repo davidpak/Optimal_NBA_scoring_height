2f307e3372cd742e128755fc2a7a82d9bbb2199e
David Pak <sungh1404@gmail.com>
2023-09-12 14:58:53 -0700

    Took away redundant statistics and changed the weights of the existing ones.

--- main.py
+++ main.py
@@ -26,59 +26,41 @@ def print_score(_dict, stat):
 
 def get_best_scorers():
     global final_scores
-    df_fgp = pd.read_csv('data/FGP.csv')
     df_ppg = pd.read_csv('data/PPG.csv')
     df_pts = pd.read_csv('data/PTS.csv')
     df_tsp = pd.read_csv('data/TSP.csv')
-    df_3pp = pd.read_csv('data/3PP.csv')
-    df_ftp = pd.read_csv('data/FTP.csv')
     df_ows = pd.read_csv('data/OWS.csv')
 
     # Subtract one from the ranks to better fit the calculations
-    df_fgp['Rank'] = df_fgp['Rank'] - 1
     df_ppg['Rank'] = df_ppg['Rank'] - 1
     df_pts['Rank'] = df_pts['Rank'] - 1
     df_tsp['Rank'] = df_tsp['Rank'] - 1
-    df_3pp['Rank'] = df_3pp['Rank'] - 1
-    df_ftp['Rank'] = df_ftp['Rank'] - 1
     df_ows['Rank'] = df_ows['Rank'] - 1
 
     # Sort dataframe in ascending order and select the top 20 players
     top_20_ppg = df_ppg.nsmallest(20, 'Rank')
-    top_20_fgp = df_fgp.nsmallest(20, 'Rank')
     top_20_pts = df_pts.nsmallest(20, 'Rank')
     top_20_tsp = df_tsp.nsmallest(20, 'Rank')
-    top_20_3pp = df_3pp.nsmallest(20, 'Rank')
-    top_20_ftp = df_ftp.nsmallest(20, 'Rank')
     top_20_ows = df_ows.nsmallest(20, 'Rank')
 
     ppg_rank_dict = dict(zip(top_20_ppg['Player'], top_20_ppg['Rank']))
-    fgp_rank_dict = dict(zip(top_20_fgp['Player'], top_20_fgp['Rank']))
     pts_rank_dict = dict(zip(top_20_pts['Player'], top_20_pts['Rank']))
     tsp_rank_dict = dict(zip(top_20_tsp['Player'], top_20_tsp['Rank']))
-    tpp_rank_dict = dict(zip(top_20_3pp['Player'], top_20_3pp['Rank']))
-    ftp_rank_dict = dict(zip(top_20_ftp['Player'], top_20_ftp['Rank']))
     ows_rank_dict = dict(zip(top_20_ows['Player'], top_20_ows['Rank']))
 
     # Dictionaries of all the scores for each metric
-    ppg_scores = get_score(ppg_rank_dict, 35, 20)
-    fgp_scores = get_score(fgp_rank_dict, 20, 20)
-    pts_scores = get_score(pts_rank_dict, 15, 20)
-    tsp_scores = get_score(tsp_rank_dict, 5, 20)
-    tpp_scores = get_score(tpp_rank_dict, 12.5, 20)
-    ftp_scores = get_score(ftp_rank_dict, 5, 20)
-    ows_scores = get_score(ows_rank_dict, 7.5, 20)
+    ppg_scores = get_score(ppg_rank_dict, 40, 20)
+    pts_scores = get_score(pts_rank_dict, 25, 20)
+    tsp_scores = get_score(tsp_rank_dict, 25, 20)
+    ows_scores = get_score(ows_rank_dict, 10, 20)
 
     # Print scores for each individual stat
     print_score(ppg_scores, "PPG")
-    print_score(fgp_scores, "FGP")
     print_score(pts_scores, "PTS")
     print_score(tsp_scores, "TSP")
-    print_score(tpp_scores, "TPP")
     print_score(ows_scores, "OWS")
-    print_score(ftp_scores, "FTP")
 
-    all_dicts = [ppg_scores, fgp_scores, pts_scores, tsp_scores, tpp_scores, ftp_scores, ows_scores]
+    all_dicts = [ppg_scores, pts_scores, tsp_scores, ows_scores]
 
     for d in all_dicts:
         for player, score in d.items():
@@ -93,6 +75,9 @@ def get_best_scorers():
     print_score(sorted_scores, "Final")
     print(f"Player with largest score: {player_with_largest_score}, Score: {largest_score}\n")
     print("Number of players in the list:", len(sorted_scores))
+
+    # Put data into csv
+    save_dict_to_csv(sorted_scores, 'data/scoring_scores.csv', 'Player', 'Score')
 
 
 def get_heights():
@@ -132,11 +117,12 @@ def get_heights():
     height_scores = {}
     i = 0
     for height in top_scorer_heights.values():
-        height_scores[height] = height_scores.get(height, 0) + 91 - i
+        height_scores[height] = height_scores.get(height, 0) + 100 - i
         i += 1
 
     # Sort the height scores and find the optimal height
     sorted_height_scores = dict(sorted(height_scores.items(), key=lambda item: item[1], reverse=True))
+    sorted_height_scores_by_height = dict(sorted(height_scores.items(), key=lambda item: item[0], reverse=False))
     optimal_height, best_height_score = list(sorted_height_scores.items())[0]
     print_score(sorted_height_scores, "Height")
     print(f"Optimal height: {optimal_height} cm")
@@ -146,6 +132,14 @@ def get_heights():
     for player, height in top_scorer_heights.items():
         if height == optimal_height:
             print(player)
+
+    # Save heights data into a csv named "heights.csv"
+    save_dict_to_csv(sorted_height_scores_by_height, 'data/height_scores.csv', 'Height (cm)', 'Score')
+
+
+def save_dict_to_csv(heights, filename, col1, col2):
+    df = pd.DataFrame(list(heights.items()), columns=[col1, col2])
+    df.to_csv(filename, index=False)
 
 
 def main():
